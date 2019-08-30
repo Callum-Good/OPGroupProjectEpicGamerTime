2c0fb7863592079cbdc1474769d667d90f5b5a8d
mcdonhj1 <mcdonhj1@D207-61591>
2019-08-31 11:35:28 +1200

    changed games index view to show games as a table

--- GameApp/resources/views/games/index.blade.php
+++ GameApp/resources/views/games/index.blade.php
@@ -1,14 +1,32 @@
 @extends('layouts.app')
 @section('content')
     <h2 class="text-center">My Games</h2>
-    <ul class="list-group py-3 mb-3">
+   <!-- <ul class="list-group py-3 mb-3">-->
+                <table>
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Temp(read more)</th>
+                    <th>Release</th>
+                    <th>Genre</th>
+                    <th>Play Style</th>
+                    <th>Platform</th>
+                </tr>
         @forelse($games as $game)
-            <li class="list-group-item my-2">
-                <h5>{{$game->title}}</h5>
-                <p>{{str_limit($game->body,20)}}</p>
-                <small class="float-right">{{$game->created_at->diffForHumans()}}</small>
-                <a href="{{route('games.show',$game->id)}}">Read More</a>
-            </li>
+            <!--<li class="list-group-item my-2">-->
+
+                <tr>
+                    <td>{{$game->title}}</td>
+                    <td>{{str_limit($game->body,20)}}</td>
+                    <!--<small class="float-right">{{$game->created_at->diffForHumans()}}</small>-->
+                    <td><a href="{{route('games.show',$game->id)}}">Read More</a></td>
+                    <td>{{$game->release}}</td>
+                    <td>{{$game->genre}}</td>
+                    <td>{{$game->playstyle}}</td>
+                    <td>{{$game->platform}}</td>
+                </tr>
+                </table>
+            <!--</li>-->
         @empty
             <h4 class="text-center">No Games Found!</h4>
         @endforelse
