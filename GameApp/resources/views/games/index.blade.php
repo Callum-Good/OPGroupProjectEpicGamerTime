9a04f62103fe3b343c66f14a2e13d97c9f51740b
mcdonhj1 <mcdonhj1@D207-61591>
2019-08-31 15:36:00 +1200

    styled the game index page

--- GameApp/resources/views/games/index.blade.php
+++ GameApp/resources/views/games/index.blade.php
@@ -1,36 +1,39 @@
 @extends('layouts.app')
 @section('content')
-    <h2 class="text-center">My Games</h2>
+    <h2 class="text-center">Games</h2>
    <!-- <ul class="list-group py-3 mb-3">-->
                 <table>
                 <tr>
                     <th>Title</th>
                     <th>Description</th>
-                    <th>Temp(read more)</th>
+                    
                     <th>Release</th>
                     <th>Genre</th>
                     <th>Perspective</th>
                     <th>Platform</th>
                 </tr>
+
         @forelse($games as $game)
+
             <!--<li class="list-group-item my-2">-->
 
-                <tr>
-                    <td>{{$game->title}}</td>
-                    <td>{{str_limit($game->body,20)}}</td>
+            <tr>
+                    <td><a class="gpLink" href="{{route('games.show',$game->id)}}">{{$game->title}}</a></td>
+                    <td><a class="gpLink" href="{{route('games.show',$game->id)}}">{{str_limit($game->body,20)}} </a></td>
                     <!--<small class="float-right">{{$game->created_at->diffForHumans()}}</small>-->
-                    <td><a href="{{route('games.show',$game->id)}}">Read More</a></td>
-                    <td>{{$game->release}}</td>
-                    <td>{{$game->genre}}</td>
-                    <td>{{$game->perspective}}</td>
-                    <td>{{$game->platform}}</td>
+                    
+                    <td><a class="gpLink" href="{{route('games.show',$game->id)}}">{{$game->release}}</a></td>
+                    <td><a class="gpLink" href="{{route('games.show',$game->id)}}">{{$game->genre}}</a></td>
+                    <td><a class="gpLink" href="{{route('games.show',$game->id)}}">{{$game->perspective}}</a></td>
+                    <td><a class="gpLink" href="{{route('games.show',$game->id)}}">{{$game->platform}}</a></td>
                 </tr>
-                </table>
+ 
             <!--</li>-->
         @empty
             <h4 class="text-center">No Games Found!</h4>
+            </table>
         @endforelse
-    </ul>
+</table>
 
     <div class="d-flex justify-content-center">
         {{$games->links('vendor.pagination.bootstrap-4')}} {{-- <- custom pagination view --}}
