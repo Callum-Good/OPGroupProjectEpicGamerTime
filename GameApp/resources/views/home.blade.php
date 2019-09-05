0156053923693f2b40862ca84792ba7aeafba31b
mcdonhj1 <mcdonhj1@D207-61591>
2019-09-05 14:40:20 +1200

    completely restyled home page as per clients request

--- GameApp/resources/views/home.blade.php
+++ GameApp/resources/views/home.blade.php
@@ -12,15 +12,51 @@
             
         </div>
     </div>
-<!--<img src="images/twoGamers.jpg">-->
+        @guest
+    <div class="homeMenu">
+        <ul class="homeList">
+            <li><a href="{{ route('login') }}">Login</a></li>
+            <li><a href="{{ route('register') }}">Register</a></li>
+            <li><a href="{{ route('games.index') }}">Browse Games</a></li>
+            <li><a href="">Browse Groups</a></li>
+        </ul>
+    </div>
+        @else
+    <div class="homeMenulog">
+        <ul class="homeList">
+            <li><a href="{{ route('games.index') }}">Browse Games</a></li>
+            <li><a href="">Browse Groups</a></li>
+        </ul>
+    </div>
+        @endif
+    
+    <a href=""><div class="featuredGame">
+        <h3>Featured Game</h3>
+         <img class="featuredGame" src="images/ApexLegends.png">
+    </div> </a>
+
     @guest
-    <a href="{{ route('login') }}"><h3>Login</h3></a>
-    <a href="{{ route('register') }}"><h3>Register</h3></a>
-    <a href="{{ route('games.index') }}"><h3>Browse Games</h3>
-    <a href=""><h3>Browse Groups</h3>
+    <div class="yourGroups">
+        <h3>Your Groups</h3>
+        <div class=ftdGrp>
+            <p>Please login or register to see your groups</p>
+        </div>
+    </div>
     @else
-    <a href="{{ route('games.index') }}"><h3>Browse Games</h3>
-    <a href=""><h3>Browse Groups</h3>
+    <div class="yourGroups">
+    <h3>Your Groups</h3>
+        <div class=ftdGrp>
+            <h4>Bioshock Speed Run</h4>
+            <p>Top Score: 30min 2s</p>
+            <p>Player: XXxxjonnyxxXX</p>
+        </div>
+        <div class=ftdGrp>
+            <h4>PUBG Chicken Dinners</h4>
+            <p>Top Score: 100</p>
+            <p>Player: pbandj</p>
+        </div>
+    </div>
     @endif
+
 </div>
 @endsection
