df863ae5fde578377bde1f2b263dabd0e03c1a10
mcdonhj1 <mcdonhj1@D207-61591>
2019-09-10 11:01:34 +1200

    changed groups index so that it showed games in database

--- GameApp/resources/views/groups/index.blade.php
+++ GameApp/resources/views/groups/index.blade.php
@@ -4,10 +4,34 @@
                 <table>
                 <tr>
                     <th>@sortablelink('name') </th>
-                    <th>@sortablelink('game')</th>  
+                    <th>@sortablelink('game') </th>
+
                     <th>@sortablelink('type')</th>
                     <th>@sortablelink('description')</th>
-                    <th>@sortablelink('created at')</th>
-                    <th>@sortablelink('updated at')</th>
                 </tr>
+
+                @forelse($groups as $group)
+
+            <!--<li class="list-group-item my-2">-->
+
+            <tr>
+                    <td><a class="gpLink" href="{{route('groups.show',$group->id)}}">{{$group->name}}</a></td>
+                    <td><a class="gpLink" href="{{route('groups.show',$group->id)}}">{{$group->game}}</a></td>
+
+                    <td><a class="gpLink" href="{{route('groups.show',$group->id)}}">{{str_limit($group->description,20)}} </a></td>
+                    
+                    <td><a class="gpLink" href="{{route('groups.show',$group->id)}}">{{$group->type}}</a></td>
+                 
+                </tr>
+ 
+            <!--</li>-->
+        @empty
+            <h4 class="text-center">No Groups Found!</h4>
+            </table>
+        @endforelse
+</table>
+
+    <div class="d-flex justify-content-center">
+        {{$groups->links('vendor.pagination.bootstrap-4')}} {{-- <- custom pagination view --}}
+    </div>
 @endsection
