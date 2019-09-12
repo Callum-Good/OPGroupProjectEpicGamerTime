3b7090c8aad03a1f1969c0ca9fac53ad8fc07d41
Callum Good (1000029314) <GOODCC2@student.op.ac.nz>
2019-09-12 16:50:08 +1200

    created controller to add users to groups

---
+++ GameApp/app/Http/Controllers/AddToGroupController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Groups;
+use App\User;
+
+class AddToGroupController extends Controller
+{
+    //
+    $user->groups()->attach($groups);
+    return 'Success';
+}
