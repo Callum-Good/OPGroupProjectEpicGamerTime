999e820a4e4e24825c6cf89659cd76718f63855f
mcdonhj1 <mcdonhj1@student.op.ac.nz>
2019-10-17 17:15:31 +1300

    moved style sheet back

--- GameApp/resources/views/layouts/app.blade.php
+++ GameApp/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Epic Gamer Time</title>
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
@@ -18,7 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 
     <!-- Column sorting -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
