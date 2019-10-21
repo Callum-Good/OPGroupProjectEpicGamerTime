023dc35770615a612007a06488496cc886e50246
mcdonhj1 <mcdonhj1@student.op.ac.nz>
2019-10-22 12:38:02 +1300

    managed to require package and publish the vendor files, having trouble migrating though

--- GameApp/config/app.php
+++ GameApp/config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Kyslik\ColumnSortable\ColumnSortableServiceProvider::class,
+        Musonza\Groups\GroupsServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -226,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Groups' => Musonza\Groups\Facades\GroupsFacade::class,
 
     ],
 
