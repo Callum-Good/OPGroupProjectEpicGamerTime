b2a15a980f23cc1c9a329d913d1eba0f9dcf2aee
unknown <AVISGL1@student.op.ac.nz>
2019-10-27 13:41:42 +1300

    Added add and delete score functions to AddScoreToGamesController.

--- GameApp/app/Http/Controllers/AddScoreToGamesController.php
+++ GameApp/app/Http/Controllers/AddScoreToGamesController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Games;
+use App\User;
+use App\Score;
 
 class AddScoreToGamesController extends Controller
 {
-    //
+    public function addScore()
+    {
+        $formData = request()->all();
+
+        $game_id = $formData['game_id'];
+        $user_id = $formData['user_id'];
+
+        $score = New Score;
+        $score->user_id = $user_id;
+        $score->game_id = $game_id;
+
+        $game_id->save();
+
+        return redirect()
+            ->route('games.show',$game_id)
+            ->with('status','Added new highscore to game.');
+    }
+
+    public function deleteScore()
+    {
+        $formData = request()->all();
+
+        $gid = $formData['game_id'];
+        $uid = $formData['user_id'];
+    
+        $hasScore = Score::where([
+            ['game_id',$gid],
+            ['user_id', $uid]
+            ])->get();
+    
+           
+        $score = Score::findOrFail($hasScore);
+        foreach($score as $s){
+            $uGid = $s->id;
+        }
+       
+        UserGroup::where('id', $uGid)->delete();
+            
+        return redirect()
+            ->route('games.show',$gid)
+            ->with('status','Deleted highscore from game.');
+
+    }
 }
