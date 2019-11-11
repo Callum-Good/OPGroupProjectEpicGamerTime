54ea998917c0321592eac7e6a8a79985ac6ac782
mcdonhj1 <mcdonhj1@student.op.ac.nz>
2019-11-11 14:47:09 +1300

    added check for form error message in games.show for score form

--- GameApp/resources/views/games/show.blade.php
+++ GameApp/resources/views/games/show.blade.php
@@ -14,6 +14,19 @@
     @endforeach
 </div> <!-- end .flash-message -->
 
+<!--
+    Checking for any errors
+    -->
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 <div class="row">
 
     <div class="col-md-8 groupDescription">
