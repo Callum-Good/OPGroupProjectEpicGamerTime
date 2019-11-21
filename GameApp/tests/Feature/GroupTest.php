cf4b4a53aad806abe349fba1cbea02a457da91bc
Joel Glasgow <glasjg1@student.op.ac.nz>
2019-11-21 15:45:54 +1300

    added a test

---
+++ GameApp/tests/Feature/GroupTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class GroupTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/groups');
+
+        $response->assertStatus(200);
+    }
+    public function testDatabaseGame()
+    {
+        $this->
+        
+        $this->assertDatabaseHas('groups', [
+            'name' => 'Gamer Guys'
+        ]);
+
+        $this->assertDatabaseHas('groups', [
+            'game_id' => 'Sonic \'06'
+        ]);
+
+        $this->assertDatabaseHas('groups', [
+            'type' => 'Just for Fun'
+        ]);
+
+        $this->assertDatabaseHas('groups', [
+            'description' => 'A group for guys who love games'
+        ]);
+    }
+}
